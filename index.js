3abb58e2d02ef6c7a080607c6c07c6f4b84b5429
João Victor <dev.joaovictor@hotmail.com>
2023-11-01 17:42:49 -0300

    chore: add pg setup and dotenv setup

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const myVar = '';
+import express from 'express';
+import 'dotenv/config.js';
 
-console.log(myVar);
+import { PostgresHelper } from './src/db/postgres/helper.js';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  console.log('Inicio');
+  const results = await PostgresHelper.query('SELECT * FROM users;');
+  console.log(results);
+  console.log('fim');
+  res.send(JSON.stringify(results));
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
