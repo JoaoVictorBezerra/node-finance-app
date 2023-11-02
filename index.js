aeca8ebf84254ed5097a84666897d3b36b26f352
João Victor <dev.joaovictor@hotmail.com>
2023-11-01 21:40:56 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import 'dotenv/config.js';
 
-import { PostgresHelper } from './src/db/postgres/helper.js';
-
 const app = express();
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-  console.log('Inicio');
-  const results = await PostgresHelper.query('SELECT * FROM users;');
-  console.log(results);
-  console.log('fim');
-  res.send(JSON.stringify(results));
-});
+app.get('/', (req, res) =>
+  res.send(
+    JSON.stringify({
+      status: 200,
+      message: 'working!',
+      timestamp: new Date(),
+    }),
+  ),
+);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
