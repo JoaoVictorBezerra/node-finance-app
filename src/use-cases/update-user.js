b1c3a2196320c329fe450a55be13b0606a608642
João Victor <dev.joaovictor@hotmail.com>
2023-11-04 11:55:45 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,40 @@
+import { FindUserByEmailRepository } from '../repositories/postgres/find-user-by-email.js';
+import { EmailAlreadyInUseError } from '../errors/user.js';
+import bcrypt from 'bcrypt';
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js';
+
+export class UpdateUserUseCase {
+  async execute(userId, updateUserParams) {
+    const user = {
+      ...updateUserParams,
+    };
+
+    // 1. Se o email estiver sendo atualizado, vericiar se ele já existe
+    if (updateUserParams.email) {
+      // 1.1. Se o email já existir, retornar um erro
+      const findUserByEmailRepository = new FindUserByEmailRepository();
+      const userAlreadyExists = await findUserByEmailRepository.execute(
+        updateUserParams.email,
+      );
+
+      if (userAlreadyExists) {
+        throw new EmailAlreadyInUseError(updateUserParams.email);
+      }
+    }
+
+    // 2. Se a senha estiver sendo atualizada, criptografar a nova senha
+    if (updateUserParams.password) {
+      const hashedPassword = (updateUserParams.password = await bcrypt.hash(
+        updateUserParams.password,
+        10,
+      ));
+      user.password = hashedPassword;
+    }
+
+    // 3. Chamar o repository para atualizar o usuário
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+    const updateUser = await postgresUpdateUserRepository.execute(userId, user);
+
+    return updateUser;
+  }
+}
